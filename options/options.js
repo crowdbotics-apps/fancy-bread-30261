c8d234d329ad2b2e4f390028679b8935ab02128d
Crowdbotics <team@crowdbotics.com>
2021-09-02 01:40:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "fancy_bread_30261Identifier",
+  url: "https://fancy_bread_30261Identifier.botics.co",
+  api: "https://fancy_bread_30261Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
